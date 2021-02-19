e5c41a365f9d6dc546a68592d6861ba281681e7c
Jan Eileng <janarild.eileng@fhi.no>
2021-02-19 16:44:27 +0100

    dcoker compose med Restservice

---
+++ services/RestService/DemoBranch.Services.RestService/Controllers/TestRestController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DemoBranch.Services.RestService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestRestController : ControllerBase
+    {
+
+        private readonly ILogger<TestRestController> _logger;
+
+        public TestRestController(ILogger<TestRestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            var o = new
+            {
+                Nameof = nameof(TestRestController),
+                FullName = this.GetType().FullName,
+                AssemlyFullName = Assembly.GetExecutingAssembly().GetName().FullName
+            };
+
+
+            return Ok(o);
+        }
+    }
+}
