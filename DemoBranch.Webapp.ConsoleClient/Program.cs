f8e0b11cdf0e17a4e9c2b9184ec3da51d7c1b8fc
Jan Eileng <janarild.eileng@fhi.no>
2021-03-03 09:30:38 +0100

    Nswag

---
+++ DemoBranch.Webapp.ConsoleClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DemoBranch.Webapp.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var httpClient = new HttpClient();
+
+            var apiClient = new swaggerClient("https://localhost:44310/", httpClient);
+
+            // create a product
+            var result = apiClient.DemoEventAllAsync().Result;
+
+          
+
+
+
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r.ToString());
+            }
+
+         
+
+
+        }
+    }
+}
